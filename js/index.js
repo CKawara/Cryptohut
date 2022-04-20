f811ddbe529a48b04e9d05c4ab219c4c10c676ea
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-21 02:22:34 +0300

    ft: add function  to dsplay coins

--- js/index.js
+++ js/index.js
@@ -9,10 +9,57 @@ fetch('https://crypto-news-live3.p.rapidapi.com/news', options)
 .then(resp => resp.json())
 .then(data => {
     data.forEach(info => { 
-        console.log(info.url)
         displayNews(info)
     });
 })
+const baseUrl = 'https://api.coinranking.com/v2/coins?_limit=5'
+   // to avoid cors error
+const proxyUrl ='https://cors-anywhere.herokuapp.com/'
+const apky = 'coinrankingdb49bc8ef3d0298669ef651205be3bed2954f3c4a87a37fa' 
+
+
+fetch(`${proxyUrl}${baseUrl}`,{
+    method: 'GET',
+    Headers: {
+        'Content-type': 'application/json',
+        'x-access-token': `${apky }`,
+        'Access-Control-Allow-origin': '*'
+    }
+}).then(resp => {
+    if(resp.ok){
+        resp.json()
+        .then(data => {
+        data.data.coins.forEach(coin => {
+        displayCoins(coin)
+    })
+    })
+    .catch((console.error()))
+}
+    
+})
+
+
+function displayCoins(coin){
+   let coinsDiv = document.querySelector('.crypto') 
+   let div = document.createElement('div')
+   let img = document.createElement('img')
+   let p = document.createElement('span')
+   let price = document.createElement('p')
+
+   coinsDiv.id = 'coin_list' 
+   img.className = 'coinIcon'
+   div.className = 'coins'
+
+   img.src = coin.iconUrl
+   p.innerText = coin.symbol
+   price.innerText = `${coin.change}%`
+   div.appendChild(img)
+   div.appendChild(p)
+   div.appendChild(price)
+   coinsDiv.appendChild(div)
+
+}
+
 
 function displayNews(info){
     let newsDiv = document.getElementById('coin_news')
