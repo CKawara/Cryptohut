c23b8f23e872e650223b4d10838a3f105c2689f8
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-21 17:02:40 +0300

    st: add styling to news images

--- js/index.js
+++ js/index.js
@@ -85,20 +85,39 @@ window.addEventListener('DOMContentLoaded', ()=> {
 
 
     function displayNews(info){
+        let picArray = [
+            'assets/126_generated.jpg',
+            'assets/dylan-calluy-JpflvzEl5cg-unsplash.jpg',
+            'assets/jeremy-bezanger-9opiHRPIvR0-unsplash.jpg',
+            'assets/Project_55-02_prev_ui.png',
+            'assets/r1985.jpg',
+            'assets/revenue-icons-vector.png',
+            'assets/Stock_market_or_forex_trading_and_graph_on_smartphone.jpg'
+        ]
         let newsDiv = document.getElementById('coin_news')
         let div = document.createElement('div')
+        let divImg = document.createElement('div')
         let a = document.createElement('a')
         let source = document.createElement('p')
+        let img = document.createElement('img')
 
+        img.className = 'news_img'
         a.className = 'news_link'
-        div.className = 'news_div'
+        divImg.className = 'news_div'
+
+        document.querySelectorAll('.news_img').forEach(function(img) {
+            const randomPic = Math.floor((Math.random() * picArray.length));
+          img.src = picArray[randomPic];
+        });
 
         a.href = info.url
         a.innerText = info.title
         source.innerText = info.source
+        divImg.appendChild(img)
         div.appendChild(a)
         div.appendChild(source)
-        newsDiv.appendChild(div)
+        divImg.appendChild(div)
+        newsDiv.appendChild(divImg)
 
     }
 }) 
