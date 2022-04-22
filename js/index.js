12e4808ea2713f06b5523e6e99530a5cc1bf5afa
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-22 05:14:50 +0300

    ft: add alert function upon subscription

--- js/index.js
+++ js/index.js
@@ -1,67 +1,67 @@
 window.addEventListener('DOMContentLoaded', ()=> { 
     let newsDiv = document.getElementById('news_div')
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'X-RapidAPI-Host': 'crypto-news14.p.rapidapi.com',
-    //         'X-RapidAPI-Key': '9447803cb3msh4b78e7793a969f3p1c1259jsnca270ba9c9fa'
-    //     }
-    // };
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'crypto-news14.p.rapidapi.com',
+            'X-RapidAPI-Key': '9447803cb3msh4b78e7793a969f3p1c1259jsnca270ba9c9fa'
+        }
+    };
     // fetch('https://crypto-news14.p.rapidapi.com/news/cointelegraph', options)
     // .then(resp => resp.json())
-    .then(data => {
-        // default news display
-        for (let i = 0; i < 5; i++){
-            displayNews(data[i])
-        }
-        let buttons = document.getElementsByClassName("btn");
-     //returns a nodelist
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].addEventListener("click", function() {
-                // filter top stories and editors choice manually
-            if(this.innerText == 'Top Stories'){
-                newsDiv.innerHTML = ''
-                for (let i = 6; i < 10; i++){
-                    displayNews(data[i])
-                }
-            }else if (this.innerText == 'Editor\'s Choice'){
-                newsDiv.innerHTML = ''
-                for (let i = 11; i < 15; i++){
-                    displayNews(data[i])
-                }        
-            }
-            }, 
-            false);
-        }
-        for (let i = 16; i < data.length; i++){
-            displaycards(data[i])
-        }
-    })
+    // .then(data => {
+    //     // default news display
+    //     for (let i = 0; i < 5; i++){
+    //         displayNews(data[i])
+    //     }
+    //     let buttons = document.getElementsByClassName("btn");
+    //  //returns a nodelist
+    //     for (let i = 0; i < buttons.length; i++) {
+    //         buttons[i].addEventListener("click", function() {
+    //             // filter top stories and editors choice manually
+    //         if(this.innerText == 'Top Stories'){
+    //             newsDiv.innerHTML = ''
+    //             for (let i = 6; i < 10; i++){
+    //                 displayNews(data[i])
+    //             }
+    //         }else if (this.innerText == 'Editor\'s Choice'){
+    //             newsDiv.innerHTML = ''
+    //             for (let i = 11; i < 15; i++){
+    //                 displayNews(data[i])
+    //             }        
+    //         }
+    //         }, 
+    //         false);
+    //     }
+    //     for (let i = 16; i < data.length; i++){
+    //         displaycards(data[i])
+    //     }
+    // })
     const baseUrl = 'https://api.coinranking.com/v2/coins?_limit=5'
     // to avoid cors error
     const proxyUrl ='https://api.allorigins.win/raw?url='
     const apky = 'coinrankingdb49bc8ef3d0298669ef651205be3bed2954f3c4a87a37fa' 
 
 
-    fetch(`${proxyUrl}${baseUrl}`,{
-        method: 'GET',
-        Headers: {
-            'Content-type': 'application/json',
-            'x-access-token': `${apky }`,
-            'Access-Control-Allow-origin': '*'
-        }
-    }).then(resp => {
-        if(resp.ok){
-            resp.json()
-            .then(data => {
-            data.data.coins.forEach(coin => {
-            // displayCoins(coin)
-        })
-        })
-        .catch((console.error()))
-    }
+    // fetch(`${proxyUrl}${baseUrl}`,{
+    //     method: 'GET',
+    //     Headers: {
+    //         'Content-type': 'application/json',
+    //         'x-access-token': `${apky }`,
+    //         'Access-Control-Allow-origin': '*'
+    //     }
+    // }).then(resp => {
+    //     if(resp.ok){
+    //         resp.json()
+    //         .then(data => {
+    //         data.data.coins.forEach(coin => {
+    //         displayCoins(coin)
+    //     })
+    //     })
+    //     .catch((console.error()))
+    // }
         
-    })
+    // })
 
 
     function displayCoins(coin){
@@ -149,4 +149,11 @@ window.addEventListener('DOMContentLoaded', ()=> {
         columns.appendChild(card)
         news_cards.appendChild(columns)
     }
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e)=> {
+        e.preventDefault()
+        let name = document.getElementById('name').value
+        alert(`Thank you ${name} for subscribing to our newsletter!!`)
+        form.reset()
+    })
 }) 
